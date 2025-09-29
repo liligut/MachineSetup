b8efbaec77e509d94bfcfeec1c4e49f88a3cc282
liligut <61033328+liligut@users.noreply.github.com>
2025-09-29 13:40:55 -0400

    Update AddWindowsCredentials.ps1 with ip 10.0.0.20

--- scripts/AddWindowsCredentials.ps1
+++ scripts/AddWindowsCredentials.ps1
@@ -1,3 +1,4 @@
 Install-Module -Name CredentialManager -Force
 New-StoredCredential -Target "90.0.0.20" -UserName "Administrator" -Password "Administrator" -Persist Enterprise -Type DomainPassword
+New-StoredCredential -Target "10.0.0.20" -UserName "Administrator" -Password "Administrator" -Persist Enterprise -Type DomainPassword
 #New-StoredCredential -Target "90.0.0.20\Public\Trajectories" -UserName "Administrator" -Password "Administrator" -Persist Enterprise
