60510012bfd44ae3a549b1eb5463fc7bb0bb25ea
liligut <61033328+liligut@users.noreply.github.com>
2025-10-06 09:56:11 -0700

    Update install.ps1
    commented some lines

--- install.ps1
+++ install.ps1
@@ -33,10 +33,10 @@ Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -N
 #Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot 
 
 #--- Setting up Windows ---
-executeScript "FileExplorerSettings.ps1";
-executeScript "dev_app_desktop_.net.ps1";
-executeScript "browsers.ps1";
-executeScript "WUpdates.ps1";
+#executeScript "FileExplorerSettings.ps1";
+#executeScript "dev_app_desktop_.net.ps1";
+#executeScript "browsers.ps1";
+#executeScript "WUpdates.ps1";
 
 #choco install -y powershell-core
 
