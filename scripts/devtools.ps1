22e1a72a4a96233ed2611a53612799853880b1fa
liligut <61033328+liligut@users.noreply.github.com>
2025-10-08 13:43:14 -0400

    Update devtools.ps1

--- scripts/devtools.ps1
+++ scripts/devtools.ps1
@@ -14,18 +14,77 @@ function Write-Log {
 $ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
 New-Item -Path $ChocoCachePath -ItemType Directory -Force
 
-choco install --cacheLocation="$ChocoCachePath" -y cpu-z
-choco install --cacheLocation="$ChocoCachePath" -y coretemp
-choco install --cacheLocation="$ChocoCachePath" dotnetfx
-choco install --cacheLocation="$ChocoCachePath" -y vscode
-choco install --cacheLocation="$ChocoCachePath" -y sysinternals
-choco install --cacheLocation="$ChocoCachePath" beyondcompare
-choco install --cacheLocation="$ChocoCachePath" putty
-choco install sql-server-express -o -ia "'/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE'" -y
-#choco install mssqlexpress2014sp1wt -params "'/INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER'" -y
-#choco install sql-server-management-studio
-choco install --cacheLocation="$ChocoCachePath" adobereader
-choco install --cacheLocation="$ChocoCachePath" microsoft-windows-terminal
+#--- CPU-Z
+try {
+    choco install --cacheLocation="$ChocoCachePath" -y cpu-z
+    Write-Log "Successfully installed CPU-Z."
+} catch {
+    Write-Log "ERROR: Failed to install CPU-Z."
+}
+#--- CoreTemp
+try {
+    choco install --cacheLocation="$ChocoCachePath" -y coretemp
+    Write-Log "Successfully installed CoreTemp."
+} catch {
+    Write-Log "ERROR: Failed to install CoreTemp."
+}
+#--- Microsoft .NET Framework
+try {
+    choco install --cacheLocation="$ChocoCachePath" dotnetfx
+    Write-Log "Successfully installed Microsoft .NET Framework."
+} catch {
+    Write-Log "ERROR: Failed to install Microsoft .NET Framework."
+}
+#--- Microsoft .NET Framework
+try {
+    choco install --cacheLocation="$ChocoCachePath" -y vscode
+    Write-Log "Successfully installed Microsoft .NET Framework."
+} catch {
+    Write-Log "ERROR: Failed to install Microsoft .NET Framework."
+}
+#--- Sysinternals
+try {
+    choco install --cacheLocation="$ChocoCachePath" -y sysinternals
+    Write-Log "Successfully installed Sysinternals."
+} catch {
+    Write-Log "ERROR: Failed to install Sysinternals."
+}
+#--- BeyondCompare
+try {
+    choco install --cacheLocation="$ChocoCachePath" beyondcompare
+    Write-Log "Successfully installed BeyondCompare."
+} catch {
+    Write-Log "ERROR: Failed to install BeyondCompare."
+}
+#--- Putty
+try {
+    choco install --cacheLocation="$ChocoCachePath" putty
+    Write-Log "Successfully installed Putty."
+} catch {
+    Write-Log "ERROR: Failed to install Putty."
+}
+#--- SQL Server Express 
+try {
+    choco install sql-server-express -o -ia "'/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE'" -y
+    #choco install mssqlexpress2014sp1wt -params "'/INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER'" -y
+    Write-Log "Successfully installed SQL Server Express."
+} catch {
+    Write-Log "ERROR: Failed to install SQL Server Express."
+}
+#--- Adobe Reader 
+try {
+    choco install --cacheLocation="$ChocoCachePath" adobereader
+    Write-Log "Successfully installed Adobe Reader."
+} catch {
+    Write-Log "ERROR: Failed to install Adobe Reader."
+}
+#--- Microsoft Windows Terminal
+try {
+    hoco install --cacheLocation="$ChocoCachePath" microsoft-windows-terminal
+    Write-Log "Successfully installed Microsoft Windows Terminal."
+} catch {
+    Write-Log "ERROR: Failed to install Microsoft Windows Terminal."
+}
 #--- SQL Server Management Studio
 try {
     choco install sql-server-management-studio -y
@@ -33,7 +92,6 @@ try {
 } catch {
     Write-Log "ERROR: Failed to install SQL Server Management Studio."
 }
-
 #--- Git
 try {
     choco install --force git -y
@@ -41,7 +99,6 @@ try {
 } catch {
     Write-Log "ERROR: Failed to install git."
 }
-
 #--- Tortoise git
 try {
     choco install tortoisegit -y
@@ -49,6 +106,5 @@ try {
 } catch {
     Write-Log "ERROR: Failed to install Tortoise git."
 }
-
 #Refresh Environment Variables
 Get-ChildItem Env: | ForEach-Object { $_.Value = [Environment]::GetEnvironmentVariable($_.Name) }
