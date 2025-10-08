1b44498cab52383aa1a12630876831ae3d686fd3
liligut <61033328+liligut@users.noreply.github.com>
2025-10-08 12:38:28 -0400

    Update PowerConfig.ps1

--- scripts/PowerConfig.ps1
+++ scripts/PowerConfig.ps1
@@ -8,7 +8,11 @@ function Write-Log {
     Add-Content -Path $logFile -Value $fullMessage
     Write-Host $fullMessage  # Optional: Still show on console too
 }
-
-$p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'High Performance'"  
-powercfg.exe -SETACTIVE ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
-powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+try {
+    c$p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'High Performance'"  
+    powercfg.exe -SETACTIVE ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+    powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+    Write-Log "Successfully set High Performance power plan as the active power plan for the system."
+} catch {
+    Write-Log "ERROR: Failed to set High Performance power plan as the active power plan for the system."
+}
