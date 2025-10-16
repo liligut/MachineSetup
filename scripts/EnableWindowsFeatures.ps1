c6cbd39e87ddfb2e5efbb1aaf7a1892221fddcc6
liligut <61033328+liligut@users.noreply.github.com>
2025-10-16 13:35:03 -0400

    Update EnableWindowsFeatures.ps1 to use -All and -NoRestart

--- scripts/EnableWindowsFeatures.ps1
+++ scripts/EnableWindowsFeatures.ps1
@@ -11,22 +11,30 @@ function Write-Log {
 
 #>NetFramework3.5
 try {
-    Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"  
+    Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All -NoRestart  
     Write-Log "Successfully enabled NetFramework3.5."
 } catch {
     Write-Log "ERROR: Failed to enable NetFramework3.5."
 }
 #Microsoft print to PDF
 try {
-    Enable-WindowsOptionalFeature -FeatureName "Printing-PrintToPDFServices-Features" -All -Online
+    Enable-WindowsOptionalFeature -Online -FeatureName Printing-PrintToPDFServices-Features -All -NoRestart
     Write-Log "Successfully enabled Microsoft print to PDF service."
 } catch {
     Write-Log "ERROR: Failed to enable Microsoft print to PDF service."
 }
 #Telnet Client
 try {
-    Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"
-    Write-Log "Successfully enabled Telnet Client."
+    Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient -All -NoRestart
+    Write-Log "Successfully enabledTelnet Client."
 } catch {
     Write-Log "ERROR: Failed to enable Telnet Client."
+}
+#TFTP
+try {
+    Enable-WindowsOptionalFeature -Online -FeatureName TFTP -All -NoRestart
+
+    Write-Log "Successfully enabled TFTP."
+} catch {
+    Write-Log "ERROR: Failed to enable TFTP."
 }
